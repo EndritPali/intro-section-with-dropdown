81e515befd0b4a794b37d51f75f4d8bb558b258d
Endrit Pali <endritpali@gmail.com>
2024-09-16 18:52:26 +0200

    project finished

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -2,10 +2,30 @@ const featuresBtn = document.querySelector(".ft");
 const companyBtn = document.querySelector(".cmp");
 const companyDropdown = document.querySelector(".cmp--hidden");
 const featuresDropdown = document.querySelector(".ft--hidden");
+const ham = document.querySelector(".ham");
+const hiddenMenu = document.querySelector(".nav__menu");
 
 featuresBtn.addEventListener("click", () => {
-  featuresDropdown.classList.toggle('active')
+  featuresDropdown.classList.toggle("active");
 });
 companyBtn.addEventListener("click", () => {
-  companyDropdown.classList.toggle('active')
+  companyDropdown.classList.toggle("active");
 });
+
+ham.addEventListener("click", () => {
+  hiddenMenu.classList.toggle("show");
+
+  if (hiddenMenu.classList.contains("show")) {
+    ham.innerHTML = `
+               <img src="./images/icon-close-menu.svg" alt="">
+  
+     `;
+  } else {
+    ham.innerHTML = `
+      <img src="./images/icon-menu.svg" alt="">
+  
+  `;
+  }
+  
+});
+
